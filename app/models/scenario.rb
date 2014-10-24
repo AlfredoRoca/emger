a3b38388d9d9e767515650194578054da5851dff
Alfredo Roca <alfredo.roca.mas@gmail.com>
2014-10-24 15:52:15 +0000

    scenario model tests

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,4 +1,6 @@
 class Scenario < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :description, uniqueness: true
 end
--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Scenario, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    context :name do
+      it "validates presence of name" do 
+        scenario = FactoryGirl.build(:scenario, name: nil)
+
+        expect(scenario.valid?).to be false
+        expect(scenario.errors[:name].present?).to be true
+      end
+      it "validates uniqueness of name" do
+        scenario1 = FactoryGirl.create(:scenario, name: "pepe")
+        scenario2 = FactoryGirl.build(:scenario, name: "pepe")
+
+        expect(scenario2.valid?).to be false
+        expect(scenario2.errors[:name].present?).to be true
+      end
+    end
+    context :description do
+      it "validates presence of description" do
+        scenario = FactoryGirl.build(:scenario, description: nil)
+
+        expect(scenario.valid?).to be false
+        expect(scenario.errors[:description].present?).to be true
+      end
+      it "validates uniqueness of description" do 
+        scenario1 = FactoryGirl.create(:scenario, description: "escenario")
+        scenario2 = FactoryGirl.build(:scenario, description: "escenario")
+
+        expect(scenario2.valid?).to be false
+        expect(scenario2.errors[:description].present?).to be true
+      end
+    end
+  end
 end
