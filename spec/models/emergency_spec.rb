2f019481bbe15a23a4858f0c17586b2d91dde65d
Alfredo Roca <alfredo.roca.mas@gmail.com>
2014-10-24 08:40:52 +0000

    emergency model validations test.

--- spec/models/emergency_spec.rb
+++ spec/models/emergency_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Emergency, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "validates presence of date" do
+      emergency = FactoryGirl.build(:emergency, date: nil)
+
+      expect(emergency.valid?).to be false
+      expect(emergency.errors[:date].present?).to be true
+    end
+  end
 end
