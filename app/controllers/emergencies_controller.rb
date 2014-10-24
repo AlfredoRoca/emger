90cb166dd601b5009dc46768a4d58452c4ad4201
Alfredo Roca <alfredo.roca.mas@gmail.com>
2014-10-24 10:29:38 +0000

    emergency index and edit actions and views.

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,11 +1,34 @@
 class EmergenciesController < ApplicationController
+  before_action :load_emergency, only: [:edit, :update]
+
   def index
-    @emergencies = Emergency.all
+    @emergencies = Emergency.all.order("date DESC")
+  end
+
+  def edit
+  end
+
+  def show
   end
 
   def new
   end
 
-  def show
+  def update
+    if @emergency.update(emergency_params)
+      flash[:notice] = "Successfully updated emergency info..."
+      redirect_to emergencies_path
+    else
+      flash[:error] = "Sorry, update not possible. Review the errors"
+      render :edit
+    end
+  end
+
+  def emergency_params
+    params.require(:emergency).permit(:date, :status, :simulacrum)
+  end
+
+  def load_emergency
+    @emergency = Emergency.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def localize_date(date)
+    I18n.l date, format: "%d/%m/%Y %a, %H:%M" if date
+  end
 end
