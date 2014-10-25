8f0be7ca32a2531bab52e2309fb2a00cfb416208
Alfredo Roca <alfredo.roca.mas@gmail.com>
2014-10-25 08:01:35 +0000

    person model validations tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "validates presence of name" do
+      person = FactoryGirl.build(:person, name: nil)
+      expect(person.valid?).to be false
+      expect(person.errors[:name].present?).to be true
+    end
+    it "validates uniqueness of name" do
+      person1 = FactoryGirl.create(:person, name: "pepito")
+      person2 = FactoryGirl.build(:person, name: "pepito")
+      expect(person2.valid?).to be false
+      expect(person2.errors[:name].present?).to be true
+    end
+  end
 end
