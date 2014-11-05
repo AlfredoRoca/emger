b3f9c5b052b41e0a21a651cb5764a10e781934fa
Alfredo Roca <alfredo.roca.mas@gmail.com>
2014-11-05 21:18:21 +0000

    Bug fixed: if one user deletes himself, then logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     current_user_id = session[:current_user_id]
+    person = Person.find(current_user_id)
     if current_user_id
-      @current_user ||= Person.find(current_user_id)
+      @current_user ||= person
     end
+    rescue ActiveRecord::RecordNotFound
+      current_user_id = nil
+      @current_user = nil
   end
 
 end
